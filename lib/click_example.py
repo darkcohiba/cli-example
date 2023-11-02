9be067322df049aa9af205622f8a5df1cc80f228
Sam Waters <samuel.waters@gmail.com>
2023-11-02 13:26:45 -0600

    click groups

--- lib/click_example.py
+++ lib/click_example.py
@@ -7,18 +7,50 @@ import click
 
 
 # groups
+# two ways to create groups, one with the click decorator and one with the group as the decorator
+# @click.group()
+# def cli():
+#     pass
+
+# @click.command()
+# def initdb():
+#     click.echo('Initialized the database')
+
+# @click.command()
+# def dropdb():
+#     click.echo('Dropped the database')
+
+# cli.add_command(initdb)
+# cli.add_command(dropdb)
+
+# @click.group(chain=True)
+# def cli():
+#     pass
+
+# @cli.command()
+# def initdb():
+#     click.echo('Initialized the database')
+
+# @cli.command()
+# def dropdb():
+#     click.echo('Dropped the database')
+
+
+# if __name__ == '__main__':
+#     cli()
+
 
 
 # getting a user input of a single character
-click.echo('Continue? [yn] ', nl=False)
-c = click.getchar()
-click.echo()
-if c == 'y':
-    click.echo('We will go on')
-elif c == 'n':
-    click.echo('Abort!')
-else:
-    click.echo('Invalid input :(')
+# click.echo('Continue? [yn] ', nl=False)
+# c = click.getchar()
+# click.echo()
+# if c == 'y':
+#     click.echo('We will go on')
+# elif c == 'n':
+#     click.echo('Abort!')
+# else:
+#     click.echo('Invalid input :(')
 
 # progress bar
 # import time
