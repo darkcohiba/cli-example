1318b3f5b2102c9cc31e36ac7631c6d90bae01da
Sam Waters <samuel.waters@gmail.com>
2023-11-03 12:56:07 -0600

    rich

--- lib/rich_style.py
+++ lib/rich_style.py
@@ -5,12 +5,30 @@
 # pprint(":vampire:")
 
 from rich.console import Console
-console = Console()
+# console = Console()
 
-console.print([1, 2, 3])
-console.print("[blue underline]Looks like a link")
-console.print(locals())
-console.print("FOO", style="white on blue")
+
+# console.print([1, 2, 3])
+# console.print("[blue underline]Looks like a link")
+# console.print("FOO", style="white on blue")
+# # logging adds the time next to it
+# console.log("Hello, World!")
+# # output [11:59:36] Hello, World!
+
+# We can also set a width and a style variable and use it on console
+console = Console(width=20)
+# style = "bold white on blue"
+# console.print("Rich", style=style)
+# console.print("Rich", style=style, justify="left")
+# console.print("Rich", style=style, justify="center")
+# console.print("Rich", style=style, justify="right")
+
+# taking input! 
+name = console.input("What is [i]your[/i] [bold red]name[/]? :smiley: ")
+console.print(f'Hello {name}', style='italic white on black', justify='center')
+
+
+
 
 
 
