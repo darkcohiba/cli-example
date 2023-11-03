5bc364b0eeb6267eaa2708eb601e0f03c6bbf4e1
Sam Waters <samuel.waters@gmail.com>
2023-11-03 11:50:33 -0600

    rich

--- lib/rich_style.py
+++ lib/rich_style.py
@@ -1,8 +1,16 @@
-from rich import print as pprint
+# from rich import print as pprint
 
-pprint("[blue]Hello,[/blue] [red bold]Goodbye,[/red bold] have fun!")
+# pprint("[blue]Hello,[/blue] [red bold]Goodbye,[/red bold] have fun!")
 
-pprint(":vampire:")
+# pprint(":vampire:")
+
+from rich.console import Console
+console = Console()
+
+console.print([1, 2, 3])
+console.print("[blue underline]Looks like a link")
+console.print(locals())
+console.print("FOO", style="white on blue")
 
 
 
