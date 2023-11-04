897865f92e3db175aefda51d3b400555e1e4cd9b
Sam Waters <samuel.waters@gmail.com>
2023-11-04 07:11:21 -0600

    icecream

---
+++ lib/icecream_example.py
@@ -0,0 +1,27 @@
+from icecream import ic
+
+# variables
+# num1 = 2
+# ic(num1)
+# output ic| num1: 2
+
+# def greeting(name):
+#     return f'Hello {name}'
+
+# ic(greeting("Sam"))
+# ic| greeting("Sam"): 'Hello Sam'
+
+# def check_weight(weight, height):
+#     if height + weight > 200:
+#         return "Good"
+#     elif height + weight > 150:
+#         return 'Good Too'
+#     elif height + weight > 100:
+#         return 'Still Good'
+#     else:
+#         return 'The most good, but everything is good!'
+# ic(check_weight(100, 94))
+# output ic| check_weight(100, 94): 'Good Too'
+
+
+
